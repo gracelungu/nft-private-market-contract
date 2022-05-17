6d3b34f6213084751411ef8e991242990a69b18e
gracelbw <grace.lungu.bw@gmail.com>
2022-05-17 14:33:31 +0200

    Update the smart contract

--- contracts/PrivateMarket.sol
+++ contracts/PrivateMarket.sol
@@ -8,7 +8,8 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract PrivateMarket is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    address _owner;
+
+    address private _owner;
 
     struct TokenData {
         uint256 price;
@@ -23,10 +24,19 @@ contract PrivateMarket is ERC721URIStorage {
         string date;
     }
 
-    TokenData[] tokensData;
+    // An array to store the data of all the tokens for a quick listing of all NFTs
+    TokenData[] public tokensData;
+
+    // Mapping of owners to an array of all the tokens they own
     mapping(address => uint256[]) private ownerByTokenId;
+
+    // Mapping of tokenIds to the owner of the token
     mapping(uint256 => address) private tokenIdByOwner;
-    mapping(uint256 => TokenData[]) private tokenIdByTokenData;
+
+    // Mapping of tokenIds to the data of the token
+    mapping(uint256 => TokenData) private tokenIdByTokenData;
+
+    // Mapping of tokenIds to the comments of the token
     mapping(uint256 => Message[]) private messages;
 
     constructor() ERC721("NFTPrivateMarket", "NFTPM") {
@@ -45,26 +55,34 @@ contract PrivateMarket is ERC721URIStorage {
         string memory tokenURI,
         uint256 price,
         string memory name
-    ) public payable ownsToken returns (uint256) {
+    ) public ownsToken returns (uint256) {
+
+        // Increment to the new tokenId
         _tokenIds.increment();
 
+        // Mint a new token
         uint256 currentId = _tokenIds.current();
         _mint(msg.sender, currentId);
         _setTokenURI(currentId, tokenURI);
 
+        // Assign the token to the owner
         ownerByTokenId[msg.sender].push(currentId);
 
+        // Store the data of the token
         TokenData memory tokenData = TokenData(
             price,
             name,
             tokenURI,
             currentId
         );
-        tokenIdByTokenData[currentId].push(tokenData);
-
+        tokenIdByTokenData[currentId] = tokenData;
         tokensData.push(tokenData);
 
+        // Map the token to the owner
         tokenIdByOwner[currentId] = msg.sender;
+
+        // Get token transfer approval
+        _approve(msg.sender, currentId);
 
         return currentId;
     }
@@ -72,7 +90,7 @@ contract PrivateMarket is ERC721URIStorage {
     function getTokenData(uint256 tokenId)
         public
         view
-        returns (TokenData[] memory)
+        returns (TokenData memory)
     {
         return tokenIdByTokenData[tokenId];
     }
@@ -93,10 +111,25 @@ contract PrivateMarket is ERC721URIStorage {
         return tokensData;
     }
 
-    function purchaseToken(uint256 tokenId) public returns (uint256) {
-        tokenIdByOwner[tokenId] = msg.sender;
+    function purchaseToken(uint256 tokenId) public payable returns (uint256) {
+        // Get the token price
+        TokenData memory tokenData = tokenIdByTokenData[tokenId];
+        uint256 price = tokenData.price;
 
+        require(msg.value == price, "You must pay the full price");
+
+        // Get the token owner
+        address owner = tokenIdByOwner[tokenId];
+
+        // Transfer the token
+        _transfer(owner, msg.sender, tokenId);
+        tokenIdByOwner[tokenId] = msg.sender;
+        delete ownerByTokenId[owner][tokenId];
         ownerByTokenId[msg.sender].push(tokenId);
+
+        // Make a payment to the owner of the token
+        (bool sent,) = payable(owner).call{value:msg.value}("");
+        require(sent, "Payment failed");
 
         return tokenId;
     }
